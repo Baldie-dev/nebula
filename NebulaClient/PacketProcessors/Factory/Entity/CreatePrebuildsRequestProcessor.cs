a35db87d441c534290382ad49a4974e1c21aef83
Marius Ungureanu <therzok@gmail.com>
2021-04-19 23:33:24 +0300

    Update CreatePrebuildsRequestProcessor.cs

--- NebulaClient/PacketProcessors/Factory/Entity/CreatePrebuildsRequestProcessor.cs
+++ NebulaClient/PacketProcessors/Factory/Entity/CreatePrebuildsRequestProcessor.cs
@@ -49,7 +49,7 @@ namespace NebulaClient.PacketProcessors.Factory.Entity
                         planet.physics = new PlanetPhysics(planet);
                         planet.physics.Init();
                     }
-                    if (BeltManager.GetCargoTraffic(planet.factory.cargoTraffic) == null)
+                    if (BeltManager.GetBeltRenderingBatches(planet.factory.cargoTraffic) == null)
                     {
                         planet.factory.cargoTraffic.CreateRenderingBatches();
                     }
